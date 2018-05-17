2671e43ac8247b73a95c330ef0f36c7fd4ef7a9a
s5063638 <s5063638@bournemouth.ac.uk>
2018-05-17 20:53:06 +0100

    Device 9 Done

---
+++ Assets/Scripts/Device9.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Device9 : MonoBehaviour {
+
+    public GameObject explosion;
+    private GameObject dynamite;
+    private GameObject ball;
+
+    void Start()
+    {
+        ball = GameObject.Find("D9Ball");
+        dynamite = GameObject.Find("TNT FBX");
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Button")
+        {
+            StartCoroutine(Explode());
+        }
+    }
+
+    IEnumerator Explode()
+    {
+        yield return new WaitForSeconds(3f);
+        explosion.SetActive(true);
+        Destroy(dynamite);
+        ball.GetComponent<Rigidbody>().AddForce(Vector3.right * 2f);
+    }
+}
