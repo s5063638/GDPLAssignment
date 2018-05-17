15e2e00d5fc1ccce027979c15d1a3a2b099e706a
s5063638 <s5063638@bournemouth.ac.uk>
2018-05-17 17:46:21 +0100

    Device 3

---
+++ Assets/Scripts/Device3.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Device3 : MonoBehaviour {
+
+    private GameObject hingeBlock;
+
+    void Start()
+    {
+        hingeBlock = GameObject.Find("HingeBlock");
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Button")
+        {
+            Destroy(hingeBlock.gameObject);
+        }
+    }
+
+}
