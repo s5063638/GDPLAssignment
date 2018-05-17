e57deff881af6f22c7eded477343bf4e9d5982d0
s5063638 <s5063638@bournemouth.ac.uk>
2018-05-17 18:20:29 +0100

    Device 4 done

---
+++ Assets/Scripts/Device4.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Device4 : MonoBehaviour {
+
+    private GameObject floor;
+	// Use this for initialization
+	void Start ()
+    {
+        floor = GameObject.Find("D4Floor");
+	}
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ball")
+        {
+            floor.GetComponent<Animator>().enabled = true;
+        }
+    }
+}
