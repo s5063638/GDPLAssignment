607ad280f62101ffaa081928c686f64fd3fe3e50
s5063638 <s5063638@bournemouth.ac.uk>
2018-05-17 13:26:00 +0100

    Device 2 Rotate

---
+++ Assets/Scripts/Device2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Device2 : MonoBehaviour {
+
+    private GameObject device;
+    private bool collided = false;
+
+    private void Start()
+    {
+        device = GameObject.Find("Pivot");
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ball")
+        {
+            Debug.Log("Collided");
+            collided = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (collided == true)
+        {
+            device.transform.Rotate(Vector3.forward, 60.0f * Time.deltaTime);
+            if(device.transform.eulerAngles.z >= 90.0f)
+            {
+                collided = false;
+            }
+        }
+    }
+}
