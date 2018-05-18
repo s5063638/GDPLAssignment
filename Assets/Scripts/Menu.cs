3f5c0a0d76541f6d7d1caf2feaa5737f6368dc62
s5063638 <s5063638@bournemouth.ac.uk>
2018-05-18 01:38:00 +0100

    Camera Anims 1

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+    public GameObject cam;
+    public GameObject menuCanvas;
+
+    public void StartGame()
+    {
+        menuCanvas.SetActive(false);
+        cam.GetComponent<Animator>().enabled = true;
+    }
+}
