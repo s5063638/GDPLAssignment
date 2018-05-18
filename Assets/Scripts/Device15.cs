c7c20ee1d511de13fb6b03b268bd0df45609657c
s5063638 <s5063638@bournemouth.ac.uk>
2018-05-18 01:09:06 +0100

    Device 15 done

---
+++ Assets/Scripts/Device15.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Device15 : MonoBehaviour {
+
+    private Rigidbody hinge;
+
+    void Start()
+    {
+        hinge = GameObject.Find("D15Hinge").GetComponent<Rigidbody>();
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ball")
+        {
+            hinge.freezeRotation = false;
+        }
+    }
+}
