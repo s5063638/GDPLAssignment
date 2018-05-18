f98b0c78d3adda4d69c93d4b5d89f7db52abc3e2
s5063638 <s5063638@bournemouth.ac.uk>
2018-05-18 15:25:29 +0100

    Final Tweaks

--- Assets/Scripts/End.cs
+++ Assets/Scripts/End.cs
@@ -5,14 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class End : MonoBehaviour {
 
-    public GameObject menuCanvas;
     private AudioSource catMeow;
-    private GameObject cam;
 
     void Start()
     {
         catMeow = GameObject.Find("cat-meow").GetComponent<AudioSource>();
-        cam = GameObject.Find("Main Camera");
     }
 
     public void OnTriggerEnter(Collider collision)
